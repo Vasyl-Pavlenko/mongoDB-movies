4c612aca0094c4819c19f51539bc9f5f22be51c9
Vasyl Pavlenko <genlus@ukr.net>
2023-07-23 14:07:11 +0300

    Second commit

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const movieRoutes = require('./routes/movie-routes');
 
 const PORT = 3000;
-const URL = "mongodb+srv://Enzo:Combo425@cluster0.tnftxqy.mongodb.net/?retryWrites=true&w=majority";
 
 const app = express();
 app.use(express.json());
 
 mongoose
-  .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((res) => console.log('Connected to MongoDB'))
   .catch((err) => console.log(`DB connection error: ${err}`));
 
